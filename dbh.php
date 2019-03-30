784260566b7a44ef50295fac543be874f823c397
willebergh <willebergh@outlook.com>
2019-03-30 21:10:09 +0100

    moved file

--- dbh.php
+++
@@ -1,7 +0,0 @@
-<?php
-  include_once "./config.php";
-  $conn = new mysqli($servername, $username, $password, $dbname);
-
-  if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-  }
